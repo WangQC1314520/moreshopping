c56dca58f38a7bc9596a29a22d5d261db86046d2
WangQC1314520 <wangqichao123456@qq.com>
2019-09-09 16:01:33 +0800

    register.js 验证及账户信息存储

--- Moreshopping/js/login.js
+++ Moreshopping/js/login.js
@@ -1,4 +1,4 @@
-$("form").validate();
+//css样式书写
 $("section .form ul li:first").click(function(){
     $(this).css("border-bottom","2px solid green")
     $("section .form ul li:last").css("border-bottom","none")
@@ -26,3 +26,54 @@ $("section .form form2 input").keypress(function(){
         $(this).css("background","none");
     };
 })
+//验证码
+var p1Obj=document.getElementsByTagName("p")[0];
+var p2Obj=document.getElementsByTagName("p")[1];
+var btn1Obj=document.getElementsByClassName("btn")[0];
+var btn2Obj=document.getElementsByClassName("btn")[1];
+btn1Obj.style.cssText=" width:64px;height:32px; "
+btn2Obj.style.cssText=" width:64px;height:32px; "
+code(p1Obj);
+code(p2Obj);
+btn1Obj.onclick=function(){
+   code(p1Obj);
+   code(p2Obj);
+}
+btn2Obj.onclick=function(){
+    code(p1Obj);
+    code(p2Obj);
+ }
+function code(obj){
+ var arr=["a","b","c","d","d","e","f","g","h","i","g","k","i","m","n","o","p","q","r","s","t","u","v","w","x","y","z",1,2,3,4,5,6,7,8,9,0]
+ var  str=""
+ for(var i=0;i<4;i++){
+    str+=arr[random(0,35)];
+ }
+ obj.innerHTML=str;
+}
+function random(min,max){
+return parseInt(Math.random()*(max-min)+min)
+}   
+
+
+//信息判断1先读取数据，数据修改之后判断，判断正确则进入主页，
+//判断错误则弹出注册信息。
+var  message=localStorage.getItem("user");
+var  account=message.split("&");
+
+$(".form1 input:eq(0)").blur(
+
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Moreshopping/js/register.js
+++ Moreshopping/js/register.js
@@ -1,7 +1,7 @@
 
 
 
-
+//验证码的输入
   var pObj=document.querySelector("p");
   var btnObj=document.getElementsByClassName("btn")[0];
   btnObj.style.cssText=" width:64px;height:32px; "
@@ -10,31 +10,40 @@
   btnObj.onclick=function(){
      code(pObj);
   }
-
-
-  var x=false;
-  var y=false;
-  var z=false;
-       
+  function code(obj){
+   var arr=["a","b","c","d","d","e","f","g","h","i","g","k","i","m","n","o","p","q","r","s","t","u","v","w","x","y","z",1,2,3,4,5,6,7,8,9,0]
+   var  str=""
+   for(var i=0;i<4;i++){
+      str+=arr[random(0,35)];
+   }
+   obj.innerHTML=str;
+ }
+ function random(min,max){
+ return parseInt(Math.random()*(max-min)+min)
+}     
+//信息的判断
+   var x=false;
+   var y=false;
+   var z=false;
 
   $("form input:eq(0)").blur(function(){
-   var str1=$("form input:eq(0)").val();
-   console.log(str1)
-    var Reg=/^1[3456789]\d{9}$/
-    if(!Reg.test(str1)){
-        alert("请输入正确的手机号")
-        
-    }else{
-       return x=true;
-    }
-})
+      var str1=$("form input:eq(0)").val();
+
+      var Reg=/^1[3|4|5|7|8]\d{9}$/;
+      if(!Reg.test(str1)){
+         alert("请输入正确的手机号")
+         return x=false;
+      }else{
+         return x=true;
+      }
+   })
 
 $("form input:eq(1)").blur(function(){
    var str2=$("form input:eq(1)").val();
-   console.log(str2)
    var Reg=/[\da-zA-Z]{6,16}/;
     if(!Reg.test(str2)){
         alert("密码为字母和数字，6~16位")
+        return y=false;
     }else{
        return y=true;
     }
@@ -42,42 +51,46 @@ $("form input:eq(1)").blur(function(){
 
 $("form input:eq(2)").blur(function(){
    var str3=$("form input:eq(2)").val();
-   if(!str3==pObj.innerHTML){
+   
+   if(str3!==pObj.innerHTML){
        alert("验证码错误")
+       return z=false;
    }else{
-      return z=true;
+       return z=true;
    }
 })
 
 
 
 //获得数据进行拼接成json字符串  然后存储到localstorage；
-//1如何才能进行拼接
-if(x&&y&&z){
-    //点击之后存储数据进行跳转；
+
+
+
+    //1点击之后存储数据进行跳转；
+   var message=""
    $(".sub").click(function(){
-      var user= $("form input:eq(0)").val();
-      var pass= $("form input:eq(1)").val();
-      //2进行拼接
-      var str=`[${user}&${pass}]`;
-     // console.log(str);
-      //3存储数据 
-      localStorage.setItem("user",str);
-     // console.log(localStorage.getItem("user"))
+     console.log(x,y,z);
+     var checkObj=document.querySelector(".check");
+      var s=checkObj.checked;
+      if(x&&y&&z&&s){
+         console.log(1111);
+         var user= $("form input:eq(0)").val();
+         var pass= $("form input:eq(1)").val();
+         //2进行拼接
+        
+          message+=`${user}&${pass}&`;
+         
+      // console.log(str);
+         //3存储数据 
+            localStorage.setItem("user",message);
+         
+      // console.log(localStorage.getItem("user"))
+        
+      }
    })
-}
 
 
 
 
-  function code(obj){
-    var arr=["a","b","c","d","d","e","f","g","h","i","g","k","i","m","n","o","p","q","r","s","t","u","v","w","x","y","z",1,2,3,4,5,6,7,8,9,0]
-    var  str=""
-    for(var i=0;i<4;i++){
-       str+=arr[random(0,35)];
-    }
-    obj.innerHTML=str;
-  }
-  function random(min,max){
-  return parseInt(Math.random()*(max-min)+min)
-}
+
+ 
