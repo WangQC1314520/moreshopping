5889a959ec1e78ec69b3ce771be4dbe70ed6131f
WangQC1314520 <wangqichao123456@qq.com>
2019-09-09 21:25:34 +0800

    register.js 注册登录简单版完成

--- Moreshopping/js/menu.js
+++ Moreshopping/js/menu.js
@@ -1,14 +1,13 @@
 
 
 $("#mainNav .margin .table ul li").mouseenter(function(){
-
-       $("#mainNav .margin .menu2").stop().show();//移入li  显示然后
+        $(this).children(".menu2").stop().show();//移入li  显示然后
 //移入menu2  显示 但从menu2移出是隐藏
-        $("#mainNav .margin .menu2").mouseleave(function(){
-        $("#mainNav .margin .menu2").stop().hide();
+        $(this).children(".menu2").mouseleave(function(){
+        $(this).stop().hide();
     })
 })
 
 $("#mainNav .margin .table ul li").mouseleave(function(){
-      // $("#mainNav .margin .menu2").css("display","none");
+    $(this).children(".menu2").stop().hide();
 })
