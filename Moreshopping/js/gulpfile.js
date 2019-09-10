6da0173dd549ce879b60f1088705912d48df95d9
WangQC1314520 <wangqichao123456@qq.com>
2019-09-10 08:02:29 +0800

    二级菜单数据准备及传输设置

--- Moreshopping/js/gulpfile.js
+++
@@ -1,7 +0,0 @@
-let sass=require("gulp-sass-china")
-
-gulp.task("sass",()=>{
-    gulp.src("../sass/*.scss")
-    .pipe(sass())
-    .pipe(gulp.dest("../css/"))
-})
--- Moreshopping/js/menu.js
+++ Moreshopping/js/menu.js
@@ -11,3 +11,68 @@ $("#mainNav .margin .table ul li").mouseenter(function(){
 $("#mainNav .margin .table ul li").mouseleave(function(){
     $(this).children(".menu2").stop().hide();
 })
+
+
+class List{
+    constructor(){
+        this.url="http://localhost/Moreshopping/data/menu.json";
+        this.menuLi=document.querySelectorAll("#mainNav .margin .table ul li");
+        console.log(this.menuLi);
+        this.load();
+        this.display();
+    }
+    // 下载数据
+    load(){
+        var that=this;
+        ajax({
+            url:this.url,
+            success:function(res){
+              //  console.log(res);
+                that.res=JSON.parse(res);
+             //   console.log(that.res[0].menu1);
+                
+            }
+        })
+    }
+    display(){
+
+    //    for(var i=0;i<this.res.length;i++){
+
+    //    }
+    }
+
+//     display(){
+//         var str="";
+//         for(var i=0;i<this.res.length;i++){
+//             str+=`<div class="goods" commodity="${this.res[i].goodsId}">
+//         <img src="date/${this.res[i].url}" alt="">
+//         <p>${this.res[i].name}物美价廉，特价出售<p>
+//         <p>价格特价出售<span>${this.res[i].price}</span></p>
+//         <p>${this.res[i].tip}</p>
+//         <i class="btn">加入购物车<i>
+//     </div> `
+
+//         }
+
+//      this.box.innerHTML=str;
+//    }
+//    //事件委托机制  获取元素添加事件
+//    addevent(){
+//        var that=this;
+//        this.box.addEventListener("click",function(eve){
+           
+//           var e=eve||window.event
+//           var target=e.target||e.srcElement
+//           if(target.className=="btn"){
+//             //  console.log(target)
+//             that.id=target.parentNode.getAttribute("commodity")
+//             that.setLocal();// start storage
+//           } 
+//        })
+//    }
+//    //点
+
+
+
+}
+new List;
